3a28bc7e3ed6af82a1aa8adc58fe5ad08733c1f1
Aleksey <lehazakharov@yandex.ru>
2018-05-30 14:32:36 +0300

    Yandex C++ course update! Week 1 is complete!

--- C++/Yandex/src/week_1/header_week_1.h
+++ C++/Yandex/src/week_1/header_week_1.h
@@ -1,20 +1,24 @@
 #ifndef HEADER_WEEK_1_H
 #define HEADER_WEEK_1_H
 
-// Функция tie() связывает для упрощения оператора сравнения (все через ссылки, поэтому нет копирования)
-// Создать tuple - make_tuple()
-// доступ к элементу get<1>(tuple_name)
 #include<iostream>
-#include<tuple> 
-#include<map>
+#include <cstdlib>
 #include<vector>
+#include<map>
+#include <string>
+#include<utility>
+#include<algorithm>
+#include<tuple> 
+#include <stdexcept> 
 
 using namespace std;
 
-#pragma region Task2
+#pragma region Трекер задач
 
-namespace w1_t2
-{
+	/*
+		Реализуйте класс TeamTasks, позволяющий хранить статистику по статусам задач команды разработчиков.
+	*/
+
 	enum class TaskStatus {
 		NEW,
 		IN_PROGRESS,
@@ -33,16 +37,82 @@ namespace w1_t2
 		void AddNewTask(const string& person);
 
 		// Обновить статусы по данному количеству задач конкретного разработчика,
-		// подробности см. ниже
 		tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string& person, int task_count);
+
+		int GetNumOfUndoneTasks(map<TaskStatus, int>& tasks) const;
+
+		void ClearMap(TasksInfo& item);
 
 	private:
 		std::map<std::string, TasksInfo> body_;
 	};
 
+#pragma endregion
+
+#pragma region Всё в квадрат
+
+/*
+	Реализуйте шаблонную функцию Sqr, которая работает не только для чисел, но и для контейнеров. 
+	Функция должна возвращать копию исходного контейнера, модифицировав его следующим образом:
+	- для vector элементы нужно возвести в квадрат;
+	- для map в квадрат нужно возвести только значения, но не ключи;
+	- для pair в квадрат нужно возвести каждый элемент пары.
+*/
+
+template<typename T> T Sqr(T val);
+template<typename T> std::vector<T> Sqr(std::vector<T> & v);
+template<typename Key, typename Val> std::map<Key, Val> Sqr(std::map<Key, Val> m);
+template<typename Key, typename Val> std::pair<Key, Val> Sqr(std::pair<Key, Val> & p);
+
+template<typename T>
+T Sqr(T val)
+{
+	return val * val;
+}
+
+template<typename T>
+std::vector<T> Sqr(std::vector<T> & v)
+{
+	std::vector<T> tmp = v;
+	for (auto & i : tmp)
+		i = Sqr(i);
+	return tmp;
+}
+
+template<typename Key, typename Val>
+std::map<Key, Val> Sqr(std::map<Key, Val> m)
+{
+	std::map<Key, Val> tmp = m;
+	for (auto & i : tmp)
+	{
+		i.second = Sqr(i.second);
+	}
+	return tmp;
+}
+
+template<typename Key, typename Val>
+std::pair<Key, Val> Sqr(std::pair<Key, Val> & p)
+{
+	return std::make_pair(Sqr(p.first), Sqr(p.second));
 }
 
 #pragma endregion
 
+#pragma region Ссылка на элемент
+
+/*
+	Реализуйте шаблонную функцию GetRefStrict, которая на вход принимает: map и значение ключа k. 
+	Если элемент по ключу k в коллекции отсутствует, то функция должна бросить исключение runtime_error, иначе вернуть ссылку на элемент в коллекции.
+*/
+
+template<class Key, class Val>
+Val & GetRefStrict(std::map<Key, Val> & m, Key key)
+{
+	if (m.count(key) == 0)
+		throw std::runtime_error("There is no such key in map");
+	return m[key];	
+}
+
+#pragma endregion
 
 #endif // ! HEADER_WEEK_1_H
