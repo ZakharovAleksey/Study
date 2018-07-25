75fccee1bfed0b63cca3c266bf76e37193bd70f5
Aleksey <lehazakharov@yandex.ru>
2018-07-25 17:54:54 +0300

    C++ Practice.

--- C++/Algorithms/src/practice/practice.cpp
+++ C++/Algorithms/src/practice/practice.cpp
@@ -1,9 +1,105 @@
 #include "practice.h"
 
 #include<iostream>
+#include<list>
 
 namespace practice
 {
+	std::vector<int> findLongestSubarrayBySum(int s, std::vector<int> arr)
+	{
+		int left_id = 0, cur_sum = 0, cur_len = 0, max_len = 0, end_id = 0;
+		for (int right_id = 0; right_id < arr.size(); ++right_id)
+		{
+			cur_sum += arr[right_id];
+
+			if (cur_sum > s)
+			{
+				while (left_id <= right_id && cur_sum > s)
+				{
+					cur_sum -= arr[left_id];
+					++left_id;
+				}
+			}
+
+			if (cur_sum == s)
+			{
+				cur_len = right_id - left_id + 1;
+				if (max_len < cur_len)
+				{
+					max_len = cur_len;
+					end_id = right_id;
+				}
+			}
+		}
+
+		if (max_len == 0)
+			return{ -1 };
+		else
+			return{ end_id + 1 - max_len + 1, end_id + 1 };
+	}
+
+	int FibTD(int numb_id, vector<int>& fib_numb)
+	{
+		// Memoization : we still use recursion, but we store already calculated values in array
+		if (fib_numb[numb_id] == -1)
+		{
+			if (numb_id <= 1)
+				fib_numb[numb_id] = numb_id;
+			else
+				fib_numb[numb_id] = FibTD(numb_id - 1, fib_numb) + FibTD(numb_id - 2, fib_numb);
+		}
+		return fib_numb[numb_id];
+	}
+
+	int FibonachiBackward(int numb_id)
+	{
+		// Memoization procedure
+		vector<int> fibonaci_values(numb_id + 1, -1);
+		return FibTD(numb_id, fibonaci_values);
+	}
+
+	int FibonachiForward(int numb_id)
+	{
+		vector<int> fibonachi_val(numb_id + 1, 0);
+		*next(begin(fibonachi_val)) = 1;
+
+		for (size_t i = 2; i <= numb_id; ++i)
+			fibonachi_val[i] = fibonachi_val[i - 1] + fibonachi_val[i - 2];
+
+		return *rbegin(fibonachi_val);
+	}
+
+	vector<int> LargestIncreasingSubsequence(const vector<int>& v)
+	{
+		{
+			// Find Subsequence
+			vector<int> subseq_len(v.size(), 1);
+			for (size_t i = 1; i < v.size(); ++i)
+				for (size_t j = 0; j < i; ++j)
+					if (v[j] < v[i])
+						subseq_len[i] = max(subseq_len[i], subseq_len[j] + 1);
+
+			auto max_len_iter = max_element(begin(subseq_len), end(subseq_len));
+			size_t last_pos = distance(begin(subseq_len), max_len_iter);
+			size_t max_len = *max_len_iter;
+			int cur_max = v[last_pos];
+
+			list<int> result;
+			result.push_front(v[last_pos]);
+
+			for (size_t pos = last_pos - 1; pos > 0 || max_len == 0; --pos)
+			{
+				if (subseq_len[pos] == max_len - 1 && v[pos] < cur_max)
+				{
+					result.push_front(v[pos]);
+					cur_max = v[pos];
+					max_len = max_len - 1;
+				}
+			}
+			return vector<int>(begin(result), end(result));
+		}
+	}
+
 	int practice::getLengthOfLongestDivisibleSubsequence(const vector<int>& numbers)
 	{
 		vector<int> divis(numbers.size(), 1);
--- C++/Algorithms/src/practice/practice.h
+++ C++/Algorithms/src/practice/practice.h
@@ -9,16 +9,35 @@ namespace practice
 {
 	using namespace std;
 
+#pragma region Common tasks
+
+	// Codefights: https://app.codesignal.com/interview-practice/task/izLStwkDr5sMS9CEm
+	std::vector<int> findLongestSubarrayBySum(int s, std::vector<int> arr);
+
+#pragma endregion
+
+
 #pragma region Dynamic Programming
 
+	int FibTD(int numb_id, vector<int> & fib_numb);
+
+	// Stepic: Backward dynamic programming example (Recursion + Memoization)
+	int FibonachiBackward(int numb_id);
+
+	// Stepic: Forward dynamic programming example (No recursion)
+	int FibonachiForward(int numb_id);
+
+	// Stepic: lingest increasing subsequence
+	vector<int> LargestIncreasingSubsequence(const vector<int> & v);
 
 	// Stepic: https://stepik.org/lesson/13257/step/5?unit=3442
 	int getLengthOfLongestDivisibleSubsequence(const vector<int> &numbers);
 
+
+
 	// Codefights: https://app.codesignal.com/interview-practice/task/oJXTWuwEZiC6FTw3A
 	// Approach to solution: on the web site
 	int climbingStairs(int stairs_number);
-
 
 	// Codefights: https://app.codesignal.com/interview-practice/task/XBWN6DYRqPrKdMZs8
 	// Approach to solution: on the web site
