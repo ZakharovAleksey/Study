1a772e01df3bd6c0e2e67520f1ce746a48050136
Aleksey <lehazakharov@yandex.ru>
2019-04-18 09:49:07 +0300

    Yandex C++. Red Belt, split string task.

--- C++/Yandex/src/main.cpp
+++ C++/Yandex/src/main.cpp
@@ -744,7 +744,67 @@ private:
 	map<MDate, set<Event>> d_body;
 };
 
-int main() {
 
+// Add class copy test from course
+
+template <typename String>
+using Group = vector<String>;
+
+template <typename String>
+using Char = typename String::value_type;
+
+template <typename String>
+vector<Group<String>> GroupHeavyStrings(vector<String> strings) {
+	if (strings.empty()) {
+		return{ {} };
+	}
+	map<set<Char<String>>, Group<String>> groupMap;
+	set<Char<String>> curKey;
+
+	for (auto& curString : strings) {
+		std::for_each(begin(curString), end(curString), [&curKey](const Char<String>& symbol) {
+			curKey.insert(symbol);
+		});
+		auto keyIter = groupMap.find(curKey);
+		if (keyIter != groupMap.end()) {
+			keyIter->second.push_back(move(curString));
+		}
+		else {
+			groupMap.insert({ move(curKey),{ move(curString) } });
+		}
+		curKey.clear();
+	}
+	vector<Group<String>> res;
+	res.reserve(groupMap.size());
+	for (auto& c : groupMap) {
+		res.push_back(move(c.second));
+	}
+	return move(res);
+}
+
+void TestGroupingABC() {
+	vector<string> strings = { "caab", "abc", "cccc", "bacc", "c" };
+	auto groups = GroupHeavyStrings(strings);
+	ASSERT_EQUAL(groups.size(), 2);
+	sort(begin(groups), end(groups));  // РџРѕСЂСЏРґРѕРє РіСЂСѓРїРї РЅРµ РёРјРµРµС‚ Р·РЅР°С‡РµРЅРёСЏ
+	ASSERT_EQUAL(groups[0], vector<string>({ "caab", "abc", "bacc" }));
+	ASSERT_EQUAL(groups[1], vector<string>({ "cccc", "c" }));
+}
+
+void TestGroupingReal() {
+	vector<string> strings = { "law", "port", "top", "laptop", "pot", "paloalto", "wall", "awl" };
+	auto groups = GroupHeavyStrings(strings);
+	ASSERT_EQUAL(groups.size(), 4);
+	sort(begin(groups), end(groups));  // РџРѕСЂСЏРґРѕРє РіСЂСѓРїРї РЅРµ РёРјРµРµС‚ Р·РЅР°С‡РµРЅРёСЏ
+	ASSERT_EQUAL(groups[0], vector<string>({ "laptop", "paloalto" }));
+	ASSERT_EQUAL(groups[1], vector<string>({ "law", "wall", "awl" }));
+	ASSERT_EQUAL(groups[2], vector<string>({ "port" }));
+	ASSERT_EQUAL(groups[3], vector<string>({ "top", "pot" }));
+}
+
+int main() {
+	TestRunner tr;
+	RUN_TEST(tr, TestGroupingABC);
+	RUN_TEST(tr, TestGroupingReal);
 	return 0;
 }
