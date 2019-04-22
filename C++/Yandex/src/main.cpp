a73c0d761d87cd59dca23a9a09c66ce1b431894c
Azakharov <azakharov@aligntech.com>
2019-04-22 19:52:59 +0300

    Yandex C++. Red belt. Update in Priority Queue task.

--- C++/Yandex/src/main.cpp
+++ C++/Yandex/src/main.cpp
@@ -9,79 +9,173 @@ using namespace log_time;
 
 #include <cstdint>
 #include<memory>
+#include<array>
 
+//template <typename T>
+//class PriorityCollection {
+//public:
+//	using Priority = size_t;
+//	using Id = size_t;
+//  using Pos = size_t;
+//
+//	static const size_t N = 1'000u;
+//	
+//  PriorityCollection() : d_curId(0u), d_maxPriority(0u), d_argMaxId(0) {}
+//
+//	Id Add(T object) {
+//    d_priorityHeap[0].push_back(move(object));
+//		d_IdIdentifier[d_curId] = make_pair(0, d_priorityHeap[0].size() - 1);
+//
+//    return d_curId++;
+//	}
+//
+//  void FillInputIds(const size_t lastId, const size_t length) {
+//    for(size_t id = 0; id < length; ++i) {
+//      d_IdIdentifier[d_curId++] = make_pair(0, lastId + id);
+//    }
+//  }
+//
+//	template <typename ObjInputIt, typename IdOutputIt>
+//	void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin) {
+//    const size_t length = std::distance(range_begin, range_end);
+//    const size_t lastId = d_priorityHeap[0].size();
+//
+//    d_priorityHeap[0]{
+//      make_move_iterator(range_begin),
+//      make_move_iterator(range_end),
+//      back_inserter(d_priorityHeap[0])
+//    };
+//		
+//    //move(range_begin, range_end, back_inserter(d_priorityHeap[0]);
+//
+//    FillInputIds(lastId, length);
+//	}
+//
+//	// Возможно вот тут переделать как-то а то дефолтный конструктор так себе
+//  bool IsValid(Id id) const {
+//    return (d_priorityHeap[d_IdIdentifier[id].first][d_IdIdentifier[id].second] == T()) ? true : false;
+//  }
+//
+//	// Получить объект по идентификатору
+//  const T& Get(Id id) const {
+//    return move(d_priorityHeap[d_IdIdentifier[id]][d_IdIdentifier[id].second]);
+//  }
+//
+//	// Увеличить приоритет объекта на 1
+//  void Promote(Id id) {
+//    Priority newPriority = d_IdIdentifier[id].first + 1;
+//    d_priorityHeap[newPriority].push_back(move(d_priorityHeap[d_IdIdentifier[id].first][d_IdIdentifier[id].second]));
+//    d_IdIdentifier[id] = make_pair(newPriority, d_priorityHeap[newPriority].size() - 1);
+//    d_maxPriority = std::max(d_maxPriority, newPriority);
+//    d_argMaxId = std::max(d_argMaxId, id);
+//  }
+//
+//	// Получить объект с максимальным приоритетом и его приоритет
+//  pair<const T&, int> GetMax() const {
+//    return { d_priorityHeap[d_maxPriority][d_IdIdentifier[d_argMaxId].second], d_maxPriority };
+//  }
+//
+//	// Аналогично GetMax, но удаляет элемент из контейнера
+//  pair<T, int> PopMax() {
+//    return { move(d_priorityHeap[d_maxPriority][d_IdIdentifier[d_argMaxId].second]), d_maxPriority };
+//  }
+//
+//private:
+//  Id d_curId;
+//  Priority d_maxPriority;
+//  Id d_argMaxId;
+//
+//	map<Priority, vector<T>> d_priorityHeap;
+//	array< pair<Priority, Pos>, N> d_IdIdentifier;
+//};
 
 template <typename T>
 class PriorityCollection {
 public:
-	using Priority = size_t;
-	using Id = size_t;
-	using ObjPos = std::vector<T>::iterator;
+  using Priority = size_t;
+  using Id = /* Ñ‚Ð¸Ð¿, Ð¸ÑÐ¿Ð¾Ð»ÑŒÐ·ÑƒÐµÐ¼Ñ‹Ð¹ Ð´Ð»Ñ Ð¸Ð´ÐµÐ½Ñ‚Ð¸Ñ„Ð¸ÐºÐ°Ñ‚Ð¾Ñ€Ð¾Ð² */;
 
-	static const size_t N = 1'000'000u;
-	
-	Id Add(T object) {
-		if (d_priorityHeap.empty()) {
-			d_priorityHeap[0] = move(object);
-		}
-		else {
-			d_priorityHeap[0].push_back(move(object));
-		}
-		d_IdIdentifier[curId++] = make_pair(0, prev(end(d_priorityHeap[0])));
-	}
+  static const size_t N = 1'000'000;
 
-	// Добавить все элементы диапазона [range_begin, range_end)
-	// с помощью перемещения, записав выданные им идентификаторы
-	// в диапазон [ids_begin, ...)
-	template <typename ObjInputIt, typename IdOutputIt>
-	void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin) {
-		if (d_priorityHeap.empty()) {
-			d_priorityHeap[0]{
-				make_move_iterator(range_begin),
-				make_move_iterator(range_end),
-				back_inserter(d_priorityHeap[0])
-			}
-		}
-		else {
-			
-		}
-	}
+  PriorityCollection() : d_curId(0), d_isValid(N, false) {
+    d_body.reserve(N);
+  }
 
-	// Определить, принадлежит ли идентификатор какому-либо
-	// хранящемуся в контейнере объекту
-	bool IsValid(Id id) const;
+  Id Add(T object) {
+    d_body.push_back(move(object));
+    return d_curId++;
+  }
 
-	// Получить объект по идентификатору
-	const T& Get(Id id) const;
+  template <typename ObjInputIt, typename IdOutputIt>
+  void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin) {
+    for (auto iter = range_begin; iter != range_end; ++it) {
+      *(ids_begin++) = Add(*iter);
+    }
+  }
 
-	// Увеличить приоритет объекта на 1
-	void Promote(Id id);
+  bool IsValid(Id id) const {
+    return d_isValid[id];
+  }
 
-	// Получить объект с максимальным приоритетом и его приоритет
-	pair<const T&, int> GetMax() const;
+  const T& Get(Id id) const {
+    return d_body[id];
+  }
 
-	// Аналогично GetMax, но удаляет элемент из контейнера
-	pair<T, int> PopMax();
+  void Promote(Id id) {
+    
+  }
+
+  pair<const T&, int> GetMax() const;
+
+  pair<T, int> PopMax();
 
 private:
-	// heap: [prioruty] {vector<objects>} last with Priority is in push_back
-	map<Priority, vector<T>> d_priorityHeap;
-	// Here index id -> lead to object pair<> Priorith and position in Priority vector
-	array<pair<Priority, ObjPos>, N> d_IdIdentifier;
-	size_t curId{ 0 };
-
+  size_t d_curId;
+  vector<bool> d_isValid;
+  vector<T> d_body;
+  set<pair<Priority, Id>> d_heap;
 };
 
 
+class StringNonCopyable : public string {
+public:
+  using string::string;
+  StringNonCopyable(const StringNonCopyable&) = delete;
+  StringNonCopyable(StringNonCopyable&&) = default;
+  StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+  StringNonCopyable& operator=(StringNonCopyable&&) = default;
+};
+
+void TestNoCopy() {
+  PriorityCollection<StringNonCopyable> strings;
+  const auto white_id = strings.Add("white");
+  const auto yellow_id = strings.Add("yellow");
+  const auto red_id = strings.Add("red");
+
+  strings.Promote(yellow_id);
+  for (int i = 0; i < 2; ++i) {
+    strings.Promote(red_id);
+  }
+  strings.Promote(yellow_id);
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "yellow");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "white");
+    ASSERT_EQUAL(item.second, 0);
+  }
+}
+
 int main() {
-	TestRunner tr;
-
-	vector<int> v = { 1, 4, 2, 3, 5 };
-	auto iter = v.begin();
-	make_heap(begin(v), end(v));
-	copy(begin(v), end(v), ostream_iterator<int>(cout, " "));
-	cout << endl;
-	cout << *iter << endl;
-
-	return 0;
+  TestRunner tr;
+  RUN_TEST(tr, TestNoCopy);
+  return 0;
 }
