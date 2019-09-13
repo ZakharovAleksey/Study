3a5e93add85c3a50e79d3409f46cc1dd372ee135
Azakharov <azakharov@aligntech.com>
2019-09-13 16:09:18 +0300

    [Yandex C++. Brown Belt] Week 3. T2 is complete.

--- C++/Yandex/src/3_brown_belt/3_ConstQualifierAndUniquePtr/ConstQualifierAndUniquePtr.h
+++ C++/Yandex/src/3_brown_belt/3_ConstQualifierAndUniquePtr/ConstQualifierAndUniquePtr.h
@@ -1,5 +1,34 @@
 #pragma once
 
+#include <functional>
+#include <optional>
+
 namespace BrownBeltWeek3
 {
-}
+#pragma region Task 2. LazyValue
+
+  template<typename T>
+  class LazyValue {
+    public:
+    explicit LazyValue(std::function<T()> init) : d_body(init) {}
+
+    bool HasValue() const
+    {
+      return d_object.has_value();
+    }
+
+    const T& Get() const
+    {
+      if (!d_object.has_value())
+        d_object = d_body();
+      return *d_object;
+    }
+
+    private:
+    std::function<T()> d_body;
+    mutable std::optional<T> d_object{ std::nullopt };
+  };
+
+#pragma endregion
+
+}  // namespace BrownBeltWeek3
