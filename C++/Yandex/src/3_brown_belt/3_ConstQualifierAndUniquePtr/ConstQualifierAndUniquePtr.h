09aa1ff481508fd4e9632fe1b6512b95fbadfcf1
Azakharov <azakharov@aligntech.com>
2019-09-17 15:11:35 +0300

    [Yandex C++. Brown Belt.] Week 3. Task 4 is complete
    - Concurrent map with const methods

--- C++/Yandex/src/3_brown_belt/3_ConstQualifierAndUniquePtr/ConstQualifierAndUniquePtr.h
+++ C++/Yandex/src/3_brown_belt/3_ConstQualifierAndUniquePtr/ConstQualifierAndUniquePtr.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <functional>
+#include <future>
+#include <mutex>
 #include <optional>
 
 namespace BrownBeltWeek3
 {
-#pragma region Task 2. LazyValue
-
+  // Task 2. LazyValue
   template<typename T>
   class LazyValue {
     public:
@@ -29,6 +30,108 @@ namespace BrownBeltWeek3
     mutable std::optional<T> d_object{ std::nullopt };
   };
 
-#pragma endregion
+  // Task 3. Synchronized template with constant methods
+
+  template<typename T>
+  class Synchronized {
+    public:
+    explicit Synchronized(T initial = T()) : value(std::move(initial)) {}
+
+    struct Access
+    {
+      std::lock_guard<std::mutex> d_guard;
+      T& ref_to_value;
+    };
+
+    struct ConstAccess
+    {
+      std::lock_guard<std::mutex> d_guard;
+      const T& ref_to_value;
+    };
+
+    Access GetAccess()
+    {
+      return { std::lock_guard(d_mutex), value };
+    }
+
+    ConstAccess GetAccess() const
+    {
+      return { std::lock_guard(d_mutex), value };
+    }
+
+    private:
+    T value;
+    mutable std::mutex d_mutex;
+  };
+
+  // Task 4. Concurrent map with const qualifiers
+
+  template<typename K, typename V, typename Hash = std::hash<K>>
+  class ConcurrentMap {
+    size_t getBucketId(const K& key) const
+    {
+      return hasher(key) % d_bucketCount;
+    }
+
+    public:
+    using MapType = std::unordered_map<K, V, Hash>;
+
+    struct WriteAccess
+    {
+      std::lock_guard<std::mutex> d_guard;
+      V& ref_to_value;
+    };
+
+    struct ReadAccess
+    {
+      std::lock_guard<std::mutex> d_guard;
+      const V& ref_to_value;
+    };
+
+    explicit ConcurrentMap(size_t bucket_count)
+        : d_bucketCount(bucket_count), d_maps(d_bucketCount),
+          d_mutexes(d_bucketCount)
+    {
+    }
+
+    WriteAccess operator[](const K& key)
+    {
+      return { std::lock_guard(d_mutexes[getBucketId(key)]),
+               d_maps[getBucketId(key)][key] };
+    }
+    ReadAccess At(const K& key) const
+    {
+      if (!Has(key))
+        throw std::out_of_range("Out of range");
+      else
+        return { std::lock_guard(d_mutexes[getBucketId(key)]),
+                 d_maps[getBucketId(key)].at(key) };
+    }
+
+    bool Has(const K& key) const
+    {
+      std::lock_guard<std::mutex> g(d_mutexes[getBucketId(key)]);
+      return d_maps[getBucketId(key)].find(key) !=
+             d_maps[getBucketId(key)].end();
+    }
+
+    MapType BuildOrdinaryMap() const
+    {
+      MapType result;
+      for (size_t id = 0; id != d_bucketCount; ++id)
+      {
+        std::lock_guard<std::mutex> guard(d_mutexes[id]);
+        for (const auto& [key, value] : d_maps[id])
+          result[key] = value;
+      }
+      return result;
+    }
+
+    private:
+    Hash hasher;
+    size_t d_bucketCount{ 0u };
+    std::vector<MapType> d_maps;
+    mutable std::vector<std::mutex> d_mutexes;
+  };
 
 }  // namespace BrownBeltWeek3
