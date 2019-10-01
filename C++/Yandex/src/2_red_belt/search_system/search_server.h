cb861b16d605854210e4450866c82f0f1c58bf72
Azakharov <azakharov@aligntech.com>
2019-10-01 15:51:50 +0300

    [RB Yandex C++] Final task initial commit.

---
+++ C++/Yandex/src/2_red_belt/search_system/search_server.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <istream>
+#include <list>
+#include <map>
+#include <ostream>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace Server_NS
+{
+  class InvertedIndex {
+    public:
+    void Add(const string& document);
+    list<size_t> Lookup(const string& word) const;
+
+    const string& GetDocument(size_t id) const
+    {
+      return docs[id];
+    }
+
+    private:
+    map<string, list<size_t>> index;
+    vector<string> docs;
+  };
+
+  class SearchServer {
+    public:
+    SearchServer() = default;
+    explicit SearchServer(istream& document_input);
+    void UpdateDocumentBase(istream& document_input);
+    void AddQueriesStream(istream& query_input, ostream& search_results_output);
+
+    private:
+    InvertedIndex index;
+  };
+}  // namespace Server_NS
