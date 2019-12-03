518ef83745193d1f51d4c1b2b8cfd6ca4ffd4f57
Azakharov <azakharov@aligntech.com>
2019-12-03 19:40:49 +0300

    [ML] Yandex math. Update in plot structure
    - Update with plotter *.py file which will plot everything in future

--- Machine Learning/Yandex_specialization/0_Math/plot.py
+++ Machine Learning/Yandex_specialization/0_Math/plot.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,13 +6,12 @@ import matplotlib.pyplot as plt
 def init_plotting():
     plt.rcParams['figure.figsize'] = (10, 5)
     plt.rcParams['font.size'] = 10
-    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['font.family'] =  'fantasy' #'Times New Roman'
     plt.rcParams['axes.labelsize'] = plt.rcParams['font.size']
     plt.rcParams['axes.titlesize'] = 1.5*plt.rcParams['font.size']
     plt.rcParams['legend.fontsize'] = plt.rcParams['font.size']
     plt.rcParams['xtick.labelsize'] = plt.rcParams['font.size']
     plt.rcParams['ytick.labelsize'] = plt.rcParams['font.size']
-    # plt.rcParams['savefig.dpi'] = 2*plt.rcParams['savefig.dpi']
     plt.rcParams['xtick.major.size'] = 3
     plt.rcParams['xtick.minor.size'] = 3
     plt.rcParams['xtick.major.width'] = 1
@@ -24,16 +24,29 @@ def init_plotting():
     plt.rcParams['legend.loc'] = 'center left'
     plt.rcParams['axes.linewidth'] = 1
 
-    plt.gca().spines['right'].set_color('none')
-    plt.gca().spines['top'].set_color('none')
-    plt.gca().xaxis.set_ticks_position('bottom')
-    plt.gca().yaxis.set_ticks_position('left')
+    # plt.gca().spines['right'].set_color('none')
+    # plt.gca().spines['top'].set_color('none')
+    # plt.gca().xaxis.set_ticks_position('bottom')
+    # plt.gca().yaxis.set_ticks_position('left')
+
+
+def plot_func():
+    fig, ax = plt.subplots(1, 2)
+    x = np.arange(0, 10, 1)
+    ax[0].plot(x, x**2, linestyle='-', marker='.', linewidth=1, color='r', label=r'Выпуклая функция')
+    ax[1].plot(x, x**2, linestyle='-', marker='.', linewidth=1, color='b', label=r'Вогнутая функция')
+    
+    for cur_ax in ax:
+        handles, labels = cur_ax.get_legend_handles_labels()
+        cur_ax.legend(handles, labels)
+        cur_ax.grid(True)
+
+    plt.show()
 
 
 if __name__ == '__main__':
+    param = str(sys.argv[1])
     init_plotting()
 
-    fig, ax = plt.subplots(1, 1,)
-    x = np.arange(0, 10, 1)
-    ax.plot(x, x**2)
-    plt.show()
+    if param == 'func':
+        plot_func()
