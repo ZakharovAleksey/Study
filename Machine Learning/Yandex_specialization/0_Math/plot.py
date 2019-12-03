3e9883dbf50d73426f8fb364e4b05b0569ee42b2
Azakharov <azakharov@aligntech.com>
2019-12-03 19:03:13 +0300

    [ML] Yandex math. First notes concerning deriviatives.

---
+++ Machine Learning/Yandex_specialization/0_Math/plot.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def init_plotting():
+    plt.rcParams['figure.figsize'] = (10, 5)
+    plt.rcParams['font.size'] = 10
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['axes.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['axes.titlesize'] = 1.5*plt.rcParams['font.size']
+    plt.rcParams['legend.fontsize'] = plt.rcParams['font.size']
+    plt.rcParams['xtick.labelsize'] = plt.rcParams['font.size']
+    plt.rcParams['ytick.labelsize'] = plt.rcParams['font.size']
+    # plt.rcParams['savefig.dpi'] = 2*plt.rcParams['savefig.dpi']
+    plt.rcParams['xtick.major.size'] = 3
+    plt.rcParams['xtick.minor.size'] = 3
+    plt.rcParams['xtick.major.width'] = 1
+    plt.rcParams['xtick.minor.width'] = 1
+    plt.rcParams['ytick.major.size'] = 3
+    plt.rcParams['ytick.minor.size'] = 3
+    plt.rcParams['ytick.major.width'] = 1
+    plt.rcParams['ytick.minor.width'] = 1
+    plt.rcParams['legend.frameon'] = False
+    plt.rcParams['legend.loc'] = 'center left'
+    plt.rcParams['axes.linewidth'] = 1
+
+    plt.gca().spines['right'].set_color('none')
+    plt.gca().spines['top'].set_color('none')
+    plt.gca().xaxis.set_ticks_position('bottom')
+    plt.gca().yaxis.set_ticks_position('left')
+
+
+if __name__ == '__main__':
+    init_plotting()
+
+    fig, ax = plt.subplots(1, 1,)
+    x = np.arange(0, 10, 1)
+    ax.plot(x, x**2)
+    plt.show()
