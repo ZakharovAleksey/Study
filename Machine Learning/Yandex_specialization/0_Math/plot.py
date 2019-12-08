af0ae319733e940a3ebbf71d26a90a5809744bf4
Aleksey <lehazakharov@yandex.ru>
2019-12-08 15:14:24 +0300

    [ML] Week 2 is complete.

--- Machine Learning/Yandex_specialization/0_Math/plot.py
+++ Machine Learning/Yandex_specialization/0_Math/plot.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from numpy.linalg import eig
 
 
 def init_plotting():
@@ -63,9 +64,50 @@ def plot_func():
     plt.show()
 
 
+def plot_eigen_vetors():
+    # Obtain eigen vectors
+    A = np.array([-4, 6, -3, 5]).reshape((2, 2))
+    l, eigen_vector = eig(A)
+    eigen_A = A.dot(eigen_vector[:, 0])
+
+    # Obtain not eigen vectors
+    random_vector = np.array([-0.3, -0.1])
+    random_A = A.dot(random_vector)
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    colors = ['C{0}'.format(i) for i in range(0, 5)]
+    labels = ['СВ', 'СВ после A', 'не СВ', 'не СВ после A']
+
+    vectors = [eigen_vector[:, 0], eigen_A, random_vector, random_A]
+    vectors_shape = []
+
+    for id in range(0, len(vectors)):
+        vectors_shape.append(
+            ax.arrow(0,
+                     0,
+                     *vectors[id],
+                     width=0.03,
+                     head_width=0.08,
+                     head_length=0.1,
+                     color=colors[id],
+                     alpha=0.5))
+
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.legend(vectors_shape, labels)
+    ax.grid(True)
+    ax.set_title('Вектора до и после действия оператора A')
+    plt.show()
+
+
 if __name__ == '__main__':
     param = str(sys.argv[1])
     init_plotting()
 
     if param == 'func':
         plot_func()
+    elif param == 'eigen':
+        plot_eigen_vetors()
+    else:
+        pass
