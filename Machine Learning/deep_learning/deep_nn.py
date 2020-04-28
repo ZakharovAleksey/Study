b8d2f74a4f1cdd91efa3bad4f74e3dc5df4d19e2
Aleksey <lehazakharov@yandex.ru>
2020-04-28 17:34:57 +0300

    [DL] Add Regularization methods.
    + L2 regularization for NN
    + Simplest drop-down technique for NN

--- Machine Learning/deep_learning/deep_nn.py
+++ Machine Learning/deep_learning/deep_nn.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from sklearn.metrics import (accuracy_score, roc_auc_score)
+from enum import IntEnum
 
 import common_utils.my_datasets as my_ds
 from common_utils.visualization import plot_scatter_with_decision_boundary
@@ -36,6 +37,8 @@ class Layer:
         self.Z_cur = None
         self.A_prev = None
 
+        self.dpt_mask = None
+
     def forward(self, A_prev: np.array) -> np.array:
         """
             Forward step for layer: 
@@ -47,7 +50,8 @@ class Layer:
         self.Z_cur = self.W.dot(A_prev) + self.b
         return self.act(self.Z_cur)
 
-    def backward(self, dA_next: np.array, n_objects: int) -> np.array:
+    def backward(self, dA_next: np.array, n_objects: int,
+                 use_regularization: bool, reg_coef: float) -> np.array:
         """
             Bockward step for layer:
             1. dZ = dA * activation_deriviative(Z)
@@ -57,6 +61,9 @@ class Layer:
         dZ = dA_next * self.act_der(self.Z_cur)
 
         self.dW = dZ.dot(self.A_prev.T) / n_objects
+
+        if use_regularization:
+            self.dW = self.dW + (reg_coef * self.W) / n_objects
         assert self.dW.shape == self.W.shape
 
         self.db = np.sum(dZ, axis=1, keepdims=True) / n_objects
@@ -72,13 +79,38 @@ class Layer:
         self.W = self.W - learning_rate * self.dW
         self.b = self.b - learning_rate * self.db
 
+    def calc_weight_norm(self) -> float:
+        """
+            Calculates L2 norm for weights
+        """
+        return np.sum(self.W**2)
+
+    def generate_dpt_mask(self, dpt_threshold: float) -> np.array:
+        """
+            Generates dropdown mask for layer basing on the 
+            input threshold
+        """
+        self.dpt_mask = (np.random.rand(self.Z_cur.shape[0], self.Z_cur.shape[1]) < dpt_threshold). \
+            astype(np.int)
+
+    def get_dpt_mask(self) -> np.array:
+        return self.dpt_mask
+
+
+# Direction of propagation
+class PropType(IntEnum):
+    FORWARD = 0,
+    BACKWARD = 1
+
 
 class NeuralNetwork:
     def __init__(self,
                  layer_dims: list,
                  learning_rate: float,
                  max_epoch: int,
-                 hidden_type: LayerType = LayerType.RELU):
+                 hidden_type: LayerType = LayerType.RELU,
+                 regularization_coef: float = 0.,
+                 use_dropout: bool = False):
         self.n_layers = len(layer_dims) - 1
         self.n_hidden = self.n_layers - 1
         self.lr = learning_rate
@@ -96,14 +128,49 @@ class NeuralNetwork:
             Layer(self.n_layers, layer_dims[self.n_layers - 1],
                   layer_dims[self.n_layers], LayerType.SIGM, RANDOM_STATE))
 
-    def _forward_propagation(self, X: np.array) -> np.array:
+        # Regularization parameters
+        self.rc = regularization_coef
+        self.use_reg = self.rc != 0.
+
+        # Dropout parameters
+        self.use_dpt = use_dropout
+        self.dpt_thresh = 0.5
+
+    def __dropout_activation(self, layer: Layer, A: np.array,
+                             propagation_type: PropType) -> np.array:
+        # Mask changes on each forward propagation iteration
+        if propagation_type == PropType.FORWARD:
+            layer.generate_dpt_mask(self.dpt_thresh)
+
+        dpt_thresh_rev = 1. / self.dpt_thresh
+        # Apply dropout mask and increase activation because of
+        # several neurons dropout
+        assert A.shape == layer.get_dpt_mask().shape
+        return np.multiply(A, layer.get_dpt_mask()) * dpt_thresh_rev
+
+    def _forward_propagation(self, X: np.array,
+                             use_dropdown: bool = False) -> np.array:
         """
             Forward propagation step for NN
         """
         A = X
         for layer in self.layers:
             A = layer.forward(A)
+
+            # Apply dropdown to each layer but the last one (1 neuron only)
+            if use_dropdown and layer.id != self.n_layers:
+                A = self.__dropout_activation(layer, A, PropType.FORWARD)
         return A
+
+    def __calc_regularization_norm(self, n_obj: int):
+        """
+            Calculate weight L2 norm in regularization
+            as an additional part of loss computation
+        """
+        weigth_norm = 0.
+        for layer in self.layers:
+            weigth_norm += layer.calc_weight_norm()
+        return self.rc * weigth_norm / (2 * n_obj)
 
     def _calculate_loss(self, A_last: np.array, Y: np.array,
                         n_obj: int) -> float:
@@ -111,7 +178,13 @@ class NeuralNetwork:
             Loss calculation
         """
         log_loss = Y * np.log(A_last) + (1. - Y) * np.log(1. - A_last)
-        return -np.sum(log_loss) / n_obj
+        loss = -np.sum(log_loss) / n_obj
+
+        # In case we add L2 regularization
+        if self.use_reg:
+            loss += self.__calc_regularization_norm(n_obj)
+
+        return loss
 
     def _backward_propagation(self, A_last: np.array, Y: np.array, n_obj: int):
         """
@@ -120,7 +193,13 @@ class NeuralNetwork:
         dAL = -(np.divide(Y, A_last) - np.divide(1 - Y, 1 - A_last))
 
         for ID in reversed(range(self.n_layers)):
-            dAL = self.layers[ID].backward(dAL, n_obj)
+            dAL = self.layers[ID].backward(dAL, n_obj, self.use_reg, self.rc)
+
+            # Use the same dropdown masks for all layers but 1
+            if self.use_dpt and self.layers[ID].id != 1:
+                # Note: use layer[ID-1] for shape match
+                dAL = self.__dropout_activation(self.layers[ID - 1], dAL,
+                                                PropType.BACKWARD)
 
     def _update_weights(self):
         """
@@ -137,7 +216,7 @@ class NeuralNetwork:
 
         n_objects = X_train.shape[1]
         for ID in range(self.max_epoch):
-            A_last = self._forward_propagation(X_train)
+            A_last = self._forward_propagation(X_train, self.use_dpt)
             loss = self._calculate_loss(A_last, y_train, n_objects)
             self._backward_propagation(A_last, y_train, n_objects)
             self._update_weights()
@@ -151,7 +230,8 @@ class NeuralNetwork:
         """
         assert X_test.shape[0] == self.layers[0].W.shape[1]
 
-        y_pred = self._forward_propagation(X_test)
+        # On prediction step we never use dropdown (that's why False)
+        y_pred = self._forward_propagation(X_test, False)
         return y_pred >= self.threshold
 
 
@@ -160,7 +240,13 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = my_ds.my_moon_dataset()
     layer_dims = [X_train.shape[1], 5, 3, 1]
 
-    clf = NeuralNetwork(layer_dims, 0.05, 10000, LayerType.TANH)
+    clf = NeuralNetwork(layer_dims,
+                        0.05,
+                        10000,
+                        LayerType.TANH,
+                        regularization_coef=0.01,
+                        use_dropout=False)
+
     clf.fit(X_train.T, y_train[:, np.newaxis].T)
     y_pred = clf.predict(X_test.T)
 
